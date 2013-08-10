a6c22c2d5c2a8f9174ab92c4a7de04f3b1cb1895
clbs <chrislbs@gmail.com>
2013-08-09 21:47:23 -0500

    Adding some code to load a dictionary by a file.

--- src/com/clbs/Main.java
+++ src/com/clbs/Main.java
@@ -1,8 +1,23 @@
 package com.clbs;
 
+import com.clbs.dict.concrete.FileWordListDictionary;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final String DICTIONARY_FILE = "dictionary.txt";
+
+        // gets the current working dir
+        String cwd = System.getProperty("user.dir");
+        File dictFile = new File(cwd, DICTIONARY_FILE);
+        try {
+            FileWordListDictionary dict = new FileWordListDictionary(dictFile.getPath());
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
