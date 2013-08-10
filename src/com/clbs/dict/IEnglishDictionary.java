44fee704285f6d2158f3e3c66aacb7c8dcc9835d
clbs <chrislbs@gmail.com>
2013-08-09 19:43:02 -0500

    Adding a very basic dictionary interface.

---
+++ src/com/clbs/dict/IEnglishDictionary.java
@@ -0,0 +1,12 @@
+package com.clbs.dict;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: clbs
+ * Date: 8/9/13
+ * Time: 7:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IEnglishDictionary {
+    boolean isWord(String str);
+}
